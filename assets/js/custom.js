344dbb0e21cd7018abc50eff1b95c8d40b9b4a41
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2021-03-28 15:44:07 +0500

    added shopping cart scroll bar + styling

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,3 +6,19 @@ navbarToggler.addEventListener('click', function(){
     hiddenNavbar.classList.toggle('hide-collapse');
     hiddenNavbar.classList.toggle('show-collapse');
 })
+
+// cart functionality
+
+let hoverCart = document.querySelector('#hoverCart');
+hoverCart.addEventListener('click',function() {
+    console.log('clickable')
+    cartDetails.classList.toggle('show-cart-details');
+    hoverCart.style.display='none';
+    setTimeout(function(){
+        hoverCart.style.display='';
+    },1000)
+})
+let cart = document.querySelector('#cart');
+let cartDetails = document.querySelector('#cartDetails');
+
+//delete cart item
