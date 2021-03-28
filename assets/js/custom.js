bbd4c877e247065122bb630d553bfb036ae65462
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2021-03-28 17:31:12 +0500

    added closing and opening functionality of cart details . Also know you can delete items from cart and it also deducts from subtotal price. very cool

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,17 +8,58 @@ navbarToggler.addEventListener('click', function(){
 })
 
 // cart functionality
-
+let cartParent = document.querySelector('#cartParent');
 let hoverCart = document.querySelector('#hoverCart');
-hoverCart.addEventListener('click',function() {
+let cartDetails = document.querySelector('#cartDetails');
+function toggleCartDetails(){
     console.log('clickable')
     cartDetails.classList.toggle('show-cart-details');
     hoverCart.style.display='none';
     setTimeout(function(){
-        hoverCart.style.display='';
-    },1000)
+        hoverCart.style.display='block';
+    },300)
+}
+cartParent.addEventListener('click',function(){
+    toggleCartDetails();
 })
-let cart = document.querySelector('#cart');
-let cartDetails = document.querySelector('#cartDetails');
 
 //delete cart item
+var cartCross = document.getElementsByClassName('cart-item-cross');
+
+function deleteCartItem(e)
+{
+    e.parentElement.style.display="none"
+    // taking item price removing dollar sign and converting to number
+    var itemPrice = e.nextElementSibling.nextElementSibling.lastElementChild.innerHTML;
+    var arrayItem = itemPrice.split("");
+    arrayItem.shift();
+    arrayItem = arrayItem.join("");
+    var numberItem = parseInt(arrayItem);
+    console.log(`numberItem : ${numberItem}`);
+// taking subtotal price removing dollar sign and converting to number
+    var bill = document.getElementById('bill').innerHTML;
+    var arrayBill=bill.split("");
+    arrayBill.shift();
+    arrayBill = arrayBill.join("");
+    var numberBill = parseInt(arrayBill);
+    console.log(`numberBill : ${numberBill}`);
+    numberBill = numberBill - numberItem;
+    console.log(numberBill);
+    //CONVERTING BACK TO STRING AND ADDING DOLLAR SIGN
+
+    bill = String(numberBill);
+    arrayBill = bill.split("");
+    arrayBill.unshift('$');
+    bill = arrayBill.join("");
+    console.log(bill)
+
+    document.getElementById('bill').innerText = bill;
+
+    // reducing item counter
+    var itemCounter =document.querySelector('#itemCounter').innerText;
+    itemCounterArray = itemCounter.split("");
+    itemCounterArray[0]--;
+    itemCounter = itemCounterArray.join("");
+    document.querySelector('#itemCounter').innerText = itemCounter;
+    console.log(itemCounterArray);
+}
